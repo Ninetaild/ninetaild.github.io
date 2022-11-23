4ca27178dbbdd5e21dbe4a48b971db96a0b2cd9a
Ninetaild <109584051+Ninetaild@users.noreply.github.com>
2022-11-23 14:23:55 +0900

    Update 사칙연산.js

--- learning/사칙연산.js
+++ learning/사칙연산.js
@@ -1,13 +1,13 @@
 public class main
 {
-    public static void main(String[] args)
-    {
+	public static void main(String[] args)
+	{
 	double i=5.0, n=3.0, a=i/n;
 	int I=(int)i, N=(int)n;
-    int b=I*N, c=I+N, d=I-N, e=1;
-    for(int r=0;r<N;r++){
-        e=I*e;
-    }
-    System.out.println("i/n="+a+"\ni*n="+b+"\ni+n="+c+"\ni-n="+d+"\ni^n="+e);
-    }
+	int b=I*N, c=I+N, d=I-N, e=1;
+		for(int r=0;r<N;r++){
+			e=I*e;
+		}
+	System.out.println("i/n="+a+"\ni*n="+b+"\ni+n="+c+"\ni-n="+d+"\ni^n="+e);
+	}
 }
