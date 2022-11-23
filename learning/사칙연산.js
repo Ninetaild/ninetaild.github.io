fce61872a5a0cbf18a835bfc177ae34e95600bbe
Ninetaild <109584051+Ninetaild@users.noreply.github.com>
2022-11-23 18:09:39 +0900

    Update and rename 사칙연산.html to 사칙연산.js

---
+++ learning/사칙연산.js
@@ -0,0 +1,45 @@
+import.java.util.Scanner;
+class MainClass{
+    public void Set1(double a){
+        this.a=a;
+    }
+    public void Set2(double b){
+        this.b=b;
+    }
+}
+class SubClass extends MainClass{
+    int A=(int)a, B=(int)b, n=1;
+    public void sum(){
+        System.out.println("a+b="+(A+B));
+    }
+    public void subtract(){
+        System.out.println("a-b="+(A-B));
+    }
+    public void mul(){
+        System.out.println("a*b="+(A*B));
+    }
+    public void div(){
+    System.out.println("a/b="+(a/b));
+    }
+    public void dou(){
+        for(int i=0;r<B;i++){
+            n=A*n;
+        }
+    }
+}
+public class SubClass2{
+    public static void main(string[] args){
+        System.out.println("2개의 정수 입력");
+        Scanner sc=new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        SubClass sub=new SubClass();
+        sub.set(a);
+        sub.set(b);
+        sub.sum();
+        sub.subtract();
+        sub.mul();
+        sub.div();
+        sub.dou();
+    }
+}
